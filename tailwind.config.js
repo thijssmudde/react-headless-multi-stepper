4764cfe1969f7e6d173969bfee4856a4a1546d41
Thijs Smudde <tsmudde@bouwatch.nl>
2022-09-25 15:14:16 +0200

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./stories/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
