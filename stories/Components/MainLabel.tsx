a72ac90177788881376318055f8174f337dd9dad
Brian <bmeulenbelt@bouwatch.nl>
2022-10-10 12:57:15 +0200

    Prettier :)

--- stories/Components/MainLabel.tsx
+++ stories/Components/MainLabel.tsx
@@ -1,41 +1,41 @@
 import React from "react";
 import classNames from "classnames";
 import { IRenderLabel } from "../../src";
-import '../../src/MultiStepper/stepper.css'
+import "../../src/MultiStepper/stepper.css";
 
 const MainLabel: IRenderLabel = ({
-                                   activeIndex,
-                                   label,
-                                   index,
-                                   array: verticalSteps,
-                                 }) => (
-    <li
-        className={classNames(
-            "flex w-full cursor-pointer",
-            {
-              "text-violet-500 relative": index <= activeIndex,
-            },
-            index !== verticalSteps.length - 1
-                ? {
-                  "line after:h-[2px] after:w-full after:mt-[9px] after:ml-[20px] after:mr-[-20px]":
-                      true,
-                  "line--active": index < activeIndex,
-                  "after:bg-gray-300": index >= activeIndex,
-                }
-                : null,
-        )}
-    >
-      <div className="absolute flex flex-col items-center">
-        <div
-            className={classNames("orb-top", {
-              "orb-top--active before:bg-violet-500": index <= activeIndex,
-              "bg-gray-300": index > activeIndex,
-            })}
-        />
+  activeIndex,
+  label,
+  index,
+  array: verticalSteps,
+}) => (
+  <li
+    className={classNames(
+      "flex w-full cursor-pointer",
+      {
+        "text-violet-500 relative": index <= activeIndex,
+      },
+      index !== verticalSteps.length - 1
+        ? {
+            "line after:h-[2px] after:w-full after:mt-[9px] after:ml-[20px] after:mr-[-20px]":
+              true,
+            "line--active": index < activeIndex,
+            "after:bg-gray-300": index >= activeIndex,
+          }
+        : null,
+    )}
+  >
+    <div className="absolute flex flex-col items-center">
+      <div
+        className={classNames("orb-top", {
+          "orb-top--active before:bg-violet-500": index <= activeIndex,
+          "bg-gray-300": index > activeIndex,
+        })}
+      />
 
-        {label}
-      </div>
-    </li>
+      {label}
+    </div>
+  </li>
 );
 
 export { MainLabel };
--- stories/Components/SubLabel.tsx
+++ stories/Components/SubLabel.tsx
@@ -1,37 +1,37 @@
 import React from "react";
 import classNames from "classnames";
 import { IRenderLabel } from "../../src";
-import "../../src/MultiStepper/stepper.css"
+import "../../src/MultiStepper/stepper.css";
 
 const SubLabel: IRenderLabel = ({
-                                  activeIndex,
-                                  label,
-                                  index,
-                                  array: horizontalSteps,
-                                }) => (
-    <li
-        className={classNames(
-            "flex h-40 space-x-4 cursor-pointer items-center relative v_line",
-            { "text-violet-500": index <= activeIndex },
-            index !== horizontalSteps.length - 1
-                ? {
-                  "before:h-full before:mt-40": true,
+  activeIndex,
+  label,
+  index,
+  array: horizontalSteps,
+}) => (
+  <li
+    className={classNames(
+      "flex h-40 space-x-4 cursor-pointer items-center relative v_line",
+      { "text-violet-500": index <= activeIndex },
+      index !== horizontalSteps.length - 1
+        ? {
+            "before:h-full before:mt-40": true,
 
-                  "v_line--active": index < activeIndex,
-                  "before:bg-gray-300": index >= activeIndex,
-                }
-                : null,
-        )}
-    >
-      <div
-          className={classNames("w-3 h-3 rounded-full relative z-1 mr-3 orb", {
-            "orb--active": index <= activeIndex,
-            "bg-gray-300": index > activeIndex,
-          })}
-      />
+            "v_line--active": index < activeIndex,
+            "before:bg-gray-300": index >= activeIndex,
+          }
+        : null,
+    )}
+  >
+    <div
+      className={classNames("w-3 h-3 rounded-full relative z-1 mr-3 orb", {
+        "orb--active": index <= activeIndex,
+        "bg-gray-300": index > activeIndex,
+      })}
+    />
 
-      {label}
-    </li>
+    {label}
+  </li>
 );
 
 export { SubLabel };
